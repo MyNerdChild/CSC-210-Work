8a2e1181a99e3b13e84719f6db3261afdd84bd9f
Krysta Parker <69538387+MyNerdChild@users.noreply.github.com>
2022-09-11 16:59:44 -0400

    Add files via upload

---
+++ Student.cpp
@@ -0,0 +1,18 @@
+#include "Student.h"
+#include <iostream>
+
+using namespace std;
+
+Student::Student()
+{
+  cout << "Name: " << n;
+  cout << "Major: " << m;
+}
+
+Student::info()
+{
+   sting n = "Krysta Parker";
+   name = n;
+   string m = "Computer Science";
+   major = m;
+}
---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  Krysta Parker
+//  CSC 210
+//  9/9/22
+//
+
+#include <iostream>
+#include <string>
+#include "Student.h"
+
+using namespace std;
+
+int main() {
+
+  Student info;
+
+  return 0;
+}
